27465e0d5cd179d607ff24fcfc0d647580b8bf32
Світлана Поліщук <Gratika@yandex.ru>
2023-05-16 23:57:00 +0300

    проект не запускаэться

---
+++ front_vue/src/components/CardTest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      Featured
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">{{test.title}}</h5>
+      <p class="card-text">{{ test.body }}</p>
+      <a href="#" class="btn btn-primary">Розпочати</a>
+    </div>
+  </div>
+</template>
+<script>
+ export default {
+   props:{
+     test: {
+       type:{}
+     }
+
+   }
+
+ }
+</script>
+<style></style>
---
+++ front_vue/src/components/TestList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <CardTest
+            v-for="test in tests"
+            :test="test"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import CardTest from "./CardTest";
+export default {
+  components: {CardTest},
+  props:{
+    tests:{
+      type: Array,
+    }
+  }
+}
+</script>
+<style></style>
