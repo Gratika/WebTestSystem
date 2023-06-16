f542339bebd1ec08cd5bfb5ec91a1bae8e02e3ad
Gratika <gratika1@gmail.com>
2023-06-16 16:57:13 +0300

    проєкт з авторизацією хочаб запустився

--- back_java/src/main/java/com/testsystem/back_java/controllers/AuthenticationRestController.java
+++ back_java/src/main/java/com/testsystem/back_java/controllers/AuthenticationRestController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:5173/", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthenticationRestController {
